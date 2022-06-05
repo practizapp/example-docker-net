a962cf3aa44d2fe70e605e9cad88ef8301488e46
Risko <dv3.playbook88@gmail.com>
2022-06-05 15:21:00 +0700

    asp net core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace example_docker_net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
